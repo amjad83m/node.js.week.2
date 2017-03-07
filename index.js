a4a6026d4a3d3eaf040add0cedaca3bb62d7ad51
amjad83m <amjad83m@hotmail.com>
2017-03-07 01:41:43 +0100

    modified index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+/*
+* The data of the app (todo item list) is stored as JSON.
+* For simplicity, this app ignores I/O file system problems (physical or software corruption), so if it can't read from or wrire to
+the data file, then the file doesn not exist and the app creates a new one.
+* 
+*/
+
 var fs = require('fs');
 var help = require('./files/help.js');
 
 var todoList = [];
 var appDataFilePath = 'files/todos.json';
+
+const addedMsg = "\nTodo item added!";
+const removedMsg = "\nTodo item removed!";
+const resetMsg = "\nTodo app reset!";
+const updateMsg = "\nTodo item updated!";
 
 var appCommand = process.argv[2];
 
@@ -43,15 +55,19 @@ function addTodoItem() {
     
     var todoItem = new TodoItem(id, name, done);
     
+    /*
+    nesting 3 layers of validation, first for the todo item id, then for the completion of the todo item (yes / no), and last for the
+    description of the todo item (quotation marks if more than 1 word)
+    */
     if (isValidEntryId(id)) {
         if (!done) {
-        console.log("Please make sure you supply a (yes / no) option!");
+        console.log("\nPlease make sure you supply a (yes / no) option!");
         return;
         } else {
             done = done.toLowerCase();
         }
         if (process.argv[6]) {
-            console.log("There is a problem with either your (entry_text) input or the number of arguments input, please check the help section!");
+            console.log("\nThere is a problem with either your (entry_text) input or the number of arguments input, please check the help section!");
             return;
         }
         switch (done) {
@@ -62,24 +78,27 @@ function addTodoItem() {
                 // do nothing
                 break;
             default:
-                console.log("Please check your (yes / no) option entry!");
+                console.log("\nPlease check your (yes / no) option entry!");
                 return;
         }
+        
+        /*
+        on each addition, trying to read the previous data, if not (no data) then creating a new file to save the data
+        and then writing the new todo item to it.
+        */
         fs.readFile(appDataFilePath, function(err, data) {
             if (err) {
-                console.log("File does not exist, creating a new file...");
+                console.log("\nFile does not exist, creating a new file...");
                 todoList.push(todoItem);
-                fs.writeFile(appDataFilePath, JSON.stringify(todoList), function(err) {
-                    if (err) throw err;
-                    console.log("Todo item added!");
-                });
+                writeToJsonFile(appDataFilePath, todoList, addedMsg);
             } else {
+                /*
+                File exists, reading the data and storing it into the todo item array (todo list)
+                then adding the new todo item to it, and finally writing the result back to the file
+                */
                 todoList = JSON.parse(data);
                 todoList.push(todoItem);
-                fs.writeFile(appDataFilePath, JSON.stringify(todoList), function(err) {
-                    if (err) throw err;
-                    console.log("Todo item added!");
-                });
+                writeToJsonFile(appDataFilePath, todoList, addedMsg);
             }
         });
     }
@@ -92,7 +111,7 @@ function listTodoItems() {
         } else {
             todoList = JSON.parse(data);
             console.log("\nCurrent Todos List: (ID, Title, Done)");
-            console.log("=====================================");
+            console.log("=======================================");
             for (var i = 0; i < todoList.length; i++) {
             console.log(todoList[i].id + ", " + todoList[i].name + ", " + todoList[i].done);
             }
@@ -101,22 +120,28 @@ function listTodoItems() {
 }
 
 function removeTodoItem() {
-    var index = process.argv[3];
-    if (isValidEntryId(index)) {
+    var id = process.argv[3];
+    var itemFound = false;
+    /*
+    To remove a todo item, first validating the id entered (positive number > 0)
+    then checking if there is a data file (todo item list exists)
+    */
+    if (isValidEntryId(id)) {
         fs.readFile(appDataFilePath, function(err, data) {
             if (err) {
-                console.log("No todo items to remove. The list is empty!");
+                console.log("\nNo todo items to remove. The list is empty!");
             } else {
                 todoList = JSON.parse(data);
-                var todoItemIndex = index - 1;
-                if (todoItemIndex >= 0 && todoItemIndex <= todoList.length - 1) {
-                    todoList.splice(todoItemIndex, 1);
-                    fs.writeFile(appDataFilePath, JSON.stringify(todoList), function(err) {
-                        if (err) throw err;
-                        console.log("Todo item removed!")
-                    });
-                } else {
-                    console.log("The desired todo item is not found. Check the (entry_id) or see help!");
+                for (var i = 0; i < todoList.length; i++) {
+                    if (todoList[i].id == id) {
+                        itemFound = true;
+                        todoList.splice(i, 1);
+                        writeToJsonFile(appDataFilePath, todoList, removedMsg);
+                        break;
+                    }
+                }
+                if (!itemFound) {
+                    console.log("\nThe desired todo item to be removed is not found. Check the (entry_id) or see help!");
                 }
             }
         });
@@ -126,50 +151,65 @@ function removeTodoItem() {
 function resetTodoItems() {
     fs.readFile(appDataFilePath, function(err, data) {
         if (err) {
-            console.log("The list of todo items is already empty!");
+            console.log("\nThe list of todo items is already empty!");
         } else {
-            fs.unlink('files/todos.json', function(err) {
+            /*
+            To reset the app, I'm deleting the data file entirely
+            */
+            fs.unlink(appDataFilePath, function(err) {
                 if (err) throw err;
-                console.log("App was reset!");
+                console.log(resetMsg);
             })
         }
     });
 }
 
 function updateTodoItem() {
-    var index = process.argv[3];
+    var id = process.argv[3];
     var newName = process.argv[4];
+    var itemFound = false;
 
-    if (isValidEntryId(index)) {
+    if (isValidEntryId(id)) {
         if (process.argv[5]) {
-            console.log("There is a problem with your (new_entry_text), for more than 1 word please use quotation marks!");
+            console.log("\nThere is a problem with your (new_entry_text), for more than 1 word please use quotation marks!");
             return;
         }
         fs.readFile(appDataFilePath, function(err, data) {
            if (err) {
-               console.log("No todo items to update. The list is empty!");
+               console.log("\nNo todo items to update. The list is empty!");
            } else {
                todoList = JSON.parse(data);
-               var todoItemIndex = index - 1;
-               if (todoItemIndex >=0 || todoItemIndex <= todoList.length - 1) {
-                   todoList[todoItemIndex].name = newName;
-                   fs.writeFile(appDataFilePath, JSON.stringify(todoList), function(err) {
-                       if (err) throw err;
-                       console.log("Todo item updated!")
-                   })
-               } else {
-                   console.log("The desired todo item is not found. Check the (entry_id) or see help!");
+               for (var i = 0; i < todoList.length; i++) {
+                   if (todoList[i].id == id) {
+                       itemFound = true;
+                       todoList[i].name = newName;
+                       writeToJsonFile(appDataFilePath, todoList, updateMsg);
+                       break;
+                   }
+               }
+               if (!itemFound) {
+                   console.log("\nThe desired todo item to be updated is not found. Check the (entry_id) or see help!");
                }
            }
         });
     }
 }
 
+
+// a function to validate entered todo item id (should be a positive number > 0)
 function isValidEntryId(id) {
     if (isNaN(id * 1) || id <= 0) {
-        console.log("The (entry_id) entered is not valid. Please enter a positive number > 0 and try again!");
+        console.log("\nThe (entry_id) entered is not valid. Please enter a positive number > 0 and try again!");
         return false;
     } else {
         return true;
     }
+}
+
+
+function writeToJsonFile(filePath, dataToBeWritten, logMsg) {
+    fs.writeFile(filePath, JSON.stringify(dataToBeWritten), function(err) {
+        if (err) throw err;
+        console.log(logMsg);
+    });
 }
